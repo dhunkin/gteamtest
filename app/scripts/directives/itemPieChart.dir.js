a1eec240795fb85f3176d993a11c5311ea0c30a8
Dmitriy Hunkin <dhunkin@gmail.com>
2014-09-24 00:48:50 +0300

    general clearance

--- app/scripts/directives/itemPieChart.dir.js
+++ app/scripts/directives/itemPieChart.dir.js
@@ -8,42 +8,6 @@ angular
 				item: '=item'
 			},
 			link: function(scope, elem) {
-				// var data = google.visualization.arrayToDataTable([
-				//   ['Passed', 'Failed'],
-				//   ['Failed',  scope.item.failed],
-				//   ['Passed',  scope.item.passed]
-				// ]);
-
-				// var options = {
-				// 	backgroundColor: {
-				// 		fill: 'transparent'
-				// 	},
-				// 	colors: ['#ed7d31', '#70ad47'],
-				// 	legend: {
-				// 		position: 'none'
-				// 	},
-				// 	pieSliceBorderColor: '#d4d4d4',
-				// 	pieSliceText: 'value',
-				// 	pieSliceTextStyle: {
-				// 		fontSize: 10,
-				// 		color: '#000'
-				// 	},
-				// 	pieStartAngle: 45,
-				// 	tooltip: {
-				// 		text: 'both'
-				// 	},
-				// 	width: 80,
-				// 	chartArea: {
-				// 		top: 0,
-				// 		left: 0,
-				// 		width: 80,
-				// 		height: 80
-				// 	},
-				// 	enableInteractivity: false
-				// };
-
-				// var chart = new google.visualization.PieChart( elem[0] );
-				// chart.draw(data, options);
 				var failed = (scope.item && scope.item.failed) || 0;
 				var passed = (scope.item && scope.item.passed) || 0;
 
